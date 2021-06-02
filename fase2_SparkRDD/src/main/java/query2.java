7861291ae67811de891d8a52bcc64b00f89eaf42
jpmrs1313 <49274169+jpmrs1313@users.noreply.github.com>
2021-06-02 23:49:37 +0100

    Add files via upload

--- fase2_SparkRDD/src/main/java/query2.java
+++ fase2_SparkRDD/src/main/java/query2.java
@@ -15,21 +15,27 @@ public class query2 {
     public static Dataset<Row> base(SparkSession spark, Dataset<Row> title_basics, Dataset<Row> title_ratings, Dataset<Row> title_principal, Dataset<Row> name_basics){
         //Base:Nome, idade, número de títulos em que participou, intervalo de anos de atividade eclassificação média dos títulos em que participa.
 
+        //agrupar o title_basics ao title_principal atraves do id do filme
         Dataset<Row> movies = title_basics.join(title_principal, title_basics.col("tconst2").equalTo(title_principal.col("tconst")), "inner");
         movies=movies.drop("tconst2");
 
+        //agrupar os resultados da query anterior com os respetivos ratings de cada titulo
         movies = movies.join(title_ratings, movies.col("tconst").equalTo(title_ratings.col("tconst2")), "inner");
         movies=movies.drop("tconst2");
 
+        //calcular o ano do primeiro e ultimo titulo feitos por cada ator
+        //calcular a média da avaliação de todos os filmes de cada ator
         movies.createOrReplaceTempView("movies");
         movies = spark.sql("select nconst, min(startYear) as first, max(startYear) as last, avg(averageRating) as averageRating," +
                 " count(*) as n_titles  from movies group by nconst");
 
+        //agrupar os resultados da query anterior com o name_basics
         movies = movies.join(name_basics, movies.col("nconst").equalTo(name_basics.col("nconst2")),"inner");
         movies = movies.drop("nconst2");
 
         movies.createOrReplaceTempView("movies");
 
+        //calcular a idade de cada actor
         movies= spark.sql("select nconst, primaryName, n_titles, first, last, averageRating, SUBSTRING(birthYear, 0, CHAR_LENGTH(birthYear)-1) as decade, " +
                 "case when deathYear == '\\\\N' then  YEAR(current_date()) - birthYear " +
                 "else deathYear-birthYear " +
@@ -42,11 +48,13 @@ public class query2 {
     public static  Dataset<Row> hits(SparkSession spark, Dataset<Row> title_ratings,Dataset<Row> title_principal){
         //Hits:Top 10 dos títulos mais bem classificados em que participo.
 
+        //agrupar o title_principal ao title_ratings de forma a obter a avaliação por titulo
         Dataset<Row> movies = title_principal.join(title_ratings, title_principal.col("tconst").equalTo(title_ratings.col("tconst2")), "inner");
         movies=movies.drop("tconst2");
 
         movies.createOrReplaceTempView("movies");
 
+        //calcular os 10 titulos mais bem classificados em que cada actor participou
         movies = spark.sql("select * from ( select nconst, tconst, averageRating, " +
                 " row_number() over (partition by nconst order by averageRating desc) as title_actor_rank " +
                 " from movies) ranks " +
@@ -87,6 +95,7 @@ public class query2 {
     public static  Dataset<Row> friends(SparkSession spark, Dataset<Row> title_principal){
         //Friends:Conjunto de colaboradores de cada ator (i.e., outros atores que participaram nosmesmos títulos).
 
+        //calcular os actores com quem cada actor colaborou
         title_principal.createOrReplaceTempView("title_principal");
         Dataset<Row> friends = spark.sql("SELECT y.*, x.nconst as friend FROM title_principal x" +
                 "  JOIN title_principal y ON y.tconst = x.tconst" +
@@ -99,6 +108,7 @@ public class query2 {
         friends=friends.drop("job");
         friends=friends.drop("characters");
 
+        //agrupar os resultados por actor criando uma lista com os atores com quem colaborou
         friends=friends.groupBy("nconst").agg(collect_list(col("friend")).as("friends"));
 
         return friends;
@@ -117,6 +127,7 @@ public class query2 {
                 .master("local")
                 .getOrCreate();
 
+        //Leitura dos dados armazenamos na alinea 1 em .csv
         Dataset<Row> title_basics = spark.read().format("csv").option("header", "true").option("delimiter", ";").load("title.basics.csv");
 
         Dataset<Row> title_ratings = spark.read().format("csv").option("header", "true").option("delimiter", ";").load("title.ratings.csv");
@@ -134,6 +145,7 @@ public class query2 {
         System.out.println("--------------------------------Schema do name_basics--------------------------------");
         name_basics.printSchema();
 
+        //selecionar de cada dataframe os atributos que interessam usar
         title_basics = title_basics.select(title_basics.col("tconst").alias("tconst2"),
                 title_basics.col("startYear"));
 
@@ -146,29 +158,40 @@ public class query2 {
                 name_basics.col("deathYear"),
                 name_basics.col("primaryProfession"));
 
+        //guardar os dados de modo a que possamos aceder atraves das queries sql
         title_basics.createOrReplaceTempView("title_basics");
         title_ratings.createOrReplaceTempView("title_ratings");
         title_principal.createOrReplaceTempView("title_principal");
         name_basics.createOrReplaceTempView("name_basics");
         name_basics = spark.sql("select * from name_basics where primaryProfession='actress' or  primaryProfession='actor' ");
 
+        //obter Nome, idade, número de títulos em que participou, intervalo de anos de atividade eclassificação média dos títulos em que participa.
         Dataset<Row> base = base(spark, title_basics, title_ratings, title_principal, name_basics);
+        //obter Hits:Top 10 dos títulos mais bem classificados em que participou.
         Dataset<Row> hits = hits(spark, title_ratings, title_principal);
+        //obter Generation:Nomes do top 10 de atores da mesma geração (i.e., que nasceram na mesmadécada).
         Dataset<Row> friends = friends(spark, title_principal);
+        //obter Friends:Conjunto de colaboradores de cada ator (i.e., outros atores que participaram nosmesmos títulos).
         Dataset<Row> generation= generation(spark, title_ratings,title_principal, name_basics);
 
         hits=hits.withColumnRenamed("nconst", "nconst2");
         friends=friends.withColumnRenamed("nconst", "nconst2");
 
+        //agrupar os resultados dos base com o generation
         Dataset<Row> data = base.join(generation, base.col("decade").equalTo(generation.col("decade")), "inner");
 
         data=data.drop("decade");
+        //agrupar os resultados da query anterior com os hits
         data = data.join(hits, data.col("nconst").equalTo(hits.col("nconst2")), "inner");
 
+        //agrupar os resultados da query anterior com os resultados dos friends
         data = data.join(friends, data.col("nconst").equalTo(friends.col("nconst2")), "inner");
         data=data.drop("nconst2");
+
+        //guardar os dados no formato parquet
         data.write().parquet("result.parquet");
 
         data.show();
+
      }
 }
